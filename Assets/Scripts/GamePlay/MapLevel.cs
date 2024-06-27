6c0adbcbcc3154cb7e145811c3d132ecfc9d32a7
Reydan Defave <fadand85@gmail.com>
2024-06-27 16:56:10 +1100

    Doing LevelMap

---
+++ Assets/Scripts/GamePlay/MapLevel.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TowerDefence
+{
+    public class MapLevel : MonoBehaviour
+    {
+        public void EX_LoadLevel()
+        {
+            Debug.Log("Load level");
+        }
+    }
+}
